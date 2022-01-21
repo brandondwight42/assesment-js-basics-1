9192e5b413e35aa460c88eaa05075efd6a7054c2
Brandon Hunsaker <brandondwight42@gmail.com>
2022-01-21 09:51:29 -0700

    Software Development Lifecycle working

--- sldc.js
+++ sldc.js
@@ -12,6 +12,13 @@
 // Planning
 // Testing & Integration
 
+//Planning
+//Analysis of Requirements
+//Design
+//Implementation
+//Testing & Integration
+//Maintain
+
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -19,3 +26,21 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "This is where the customer's software needs are defined.  A list of requirements is made that the software must fullfill."
+
+const design = "This is where the methods for how the software will be written is figured out.  A developer may pseudocode or write the code out in everyday language to help design how the software will be implemented."
+
+const implementation = "The software code is actually written in an actual program with correct syntax so it can compile and run."
+
+const testingAndIntegration = "The software is tested to see if it will run, that it runs correctly, and that it meets the analysis of requirements.  Debugging occurs until the software runs correctly and fulfills its requirements."
+
+const maintain = "The software is inspected routinely to ensure it is still meeting its needed requirements and updated as needed if new requirements are needed."
+
+console.log(planning)
+console.log(analysisOfRequirements)
+console.log(design)
+console.log(implementation)
+console.log(testingAndIntegration)
+console.log(maintain)
+
--- variables.js
+++ variables.js
@@ -2,17 +2,17 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = 'Brandon Hunsaker'
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+let favenum = '42'
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let 
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
